a2a2db4f7f8f6dd2af0e54d0d5ba791b9f97c452
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-12-14 09:33:20 -0300

    wip: first version

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -6,13 +6,42 @@
    [helix.hooks :as hooks]
    [main.lib :refer [defnc]]))
 
-;; app
 (defnc app []
-  (let [[_state _set-state] (hooks/use-state {})]
-    (d/div
-     (d/h1 "Blog test" )
-     (d/h2 "a simple blog engine")
-     (d/p "made in clojure"))))
+  (let [[_state _set-state] (hooks/use-state {})
+        posts-list [{:title "Test" :date "2023-12-13"
+                     :body "testestestestestesteststes"}]]
+    (d/body
+     (d/div {:class-name "site-header"}
+            (d/body {:class-name "wrapper"}
+                    (d/div {:class-name "site-nav"}
+                           (d/a {:class-name "page-link" :href "#"} "Home")
+                           (d/a {:class-name "page-link" :href "#"} "Posts")
+                           (d/a {:class-name "page-link" :href "#"} "Archives")
+                           (d/a {:class-name "page-link" :href "#"} "Tags")
+                           (d/a {:class-name "page-link" :href "#"} "About"))
+
+                    (d/div
+                     (d/h1 {:class-name "site-title"}
+                           (d/a {:href "#"} "My Blog"))
+                     (d/p "Write ideas and study notes here."))))
+
+     (d/div {:class-name "wrapper"}
+            (for [{:keys [post-link
+                          body
+                          truncated
+                          date
+                          discuss
+                          title]} posts-list]
+              (d/div
+               {:key title}
+               (d/h1
+                (d/a {:href post-link} title))
+               body
+               (when truncated
+                 (d/p {} (d/a {:href post-link} "Continue reading")))
+               (when discuss
+                 (d/p {} (d/a {:href discuss} "Discuss this post here")))
+               (d/p (d/i "Published: " (d/span {:class-name "post-date"} date)))))))))
 
 ;; start your app with your React renderer
 (defn ^:export init []
